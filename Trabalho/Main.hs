44cce17c168cd1dd4e6714330399a60aeec8ecd4
Samuel Lopes <samuellopes12390@gmail.com>
2023-11-19 09:43:29 -0300

    Questão 4 feita (Verificação de uma lista de senhas) e pequenas alterações na questão 3

--- Trabalho/Main.hs
+++ Trabalho/Main.hs
@@ -11,8 +11,6 @@ addSum s = s ++ show (sum (map getDigito s))
 isDigitString :: String -> Bool -- definindo o tipo da função isDigitString
 isDigitString s = all isDigit s --usando a função all para aplicar isDigit em todos os caracteres da string s
 
--- Função principal que recebe a entrada do teclado e chama a função addSum
-
 -- Q2 #############################################################################################
 validaCartao :: String -> Bool
 validaCartao numCartao
@@ -28,11 +26,11 @@ validaCartao numCartao
 
 -- função que verifica se o caractere é uma vogal
 isVogal :: Char -> Bool
-isVogal s = elem s "aeiou" -- pode ter vogais maiúsculas?
+isVogal s = elem s "aeiou" || elem s "AEIOU" -- pode ter vogais maiúsculas? Acho que sim.
 
 -- função que verifica se o caractere é uma consoante
 isConsoante :: Char -> Bool
-isConsoante s = elem s ['A'..'Z']
+isConsoante s = (elem s ['A'..'Z'] || elem s ['a'..'z']) && not (isVogal s)
 
 --função que conta a quantidade de vogais
 contaVogais :: String -> Int 
@@ -47,8 +45,14 @@ validaCaracteres :: String -> Bool
 validaCaracteres s = and [length s >= 4, length s <= 8]
 
 -- função que valida a senha de acesso
-validaSenha :: String -> Bool
-validaSenha s = validaCaracteres s && contaVogais s == 2 && contaConsoantes s == 2
+-- Somente ou pelo menos duas vogais e consoantes ?
+validaSenha :: String -> Bool 
+validaSenha s = validaCaracteres s && contaVogais s == 2 && contaConsoantes s == 2 
+
+-- Q4 #############################################################################################
+verificaListaSenhas :: [String] -> [String]
+verificaListaSenhas [] = []
+verificaListaSenhas lista = filter validaSenha lista
 
 
 -- Função Main ##############################################################################
